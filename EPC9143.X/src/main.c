a9977059894b8c988c19eccde054e63a12dfa33d
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2020-01-21 04:14:25 -0800

    Updated project files & configuratoin

--- EPC9143.X/src/main.c
+++ EPC9143.X/src/main.c
@@ -17,7 +17,7 @@ int main(void) {
 
     volatile uint16_t timeout = 0;
      
-    init_fosc();        // Set up system oscillator for 100 MIPS operation
+    ConfigureOscillator();        // Set up system oscillator for 100 MIPS operation
     init_aclk();        // Set up Auxiliary PLL for 500 MHz (source clock to PWM module)
     init_timer1();      // Set up Timer1 as scheduler time base
     init_gpio();        // Initialize common device GPIOs
