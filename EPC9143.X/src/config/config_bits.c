c65509d29d9feb41f32c56e2cab34b1db1084d0a
Andreas Reiter - M91406 <andreas.reiter@microchip.com>
2020-01-21 04:12:36 -0800

    Added MCU configuraiton to project

---
+++ EPC9143.X/src/config/config_bits.c
@@ -0,0 +1,193 @@
+/*LICENSE ********************************************************************
+ * Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ * ***************************************************************************/
+/* @@config_bits.c
+ * ****************************************************************************
+ * File:   config_bits.c
+ * Author: M91406
+ *
+ * Description:
+ * - 
+ *  
+ * History:
+ * Created on July 03, 2017, 09:32 PM
+ ******************************************************************************/
+
+#if defined (__P33SMPS_EP2__)
+
+//------------------------------------------------------------------------------
+// DSPIC33EPxxGS202 Configuration Bit Settings
+//------------------------------------------------------------------------------
+// FSEC
+#pragma config BWRP = OFF               // Boot Segment Write-Protect bit (Boot Segment may be written)
+#pragma config BSS = DISABLED           // Boot Segment Code-Protect Level bits (No Protection (other than BWRP))
+#pragma config BSEN = OFF               // Boot Segment Control bit (No Boot Segment)
+#pragma config GWRP = OFF               // General Segment Write-Protect bit (General Segment may be written)
+#pragma config GSS = DISABLED           // General Segment Code-Protect Level bits (No Protection (other than GWRP))
+#pragma config CWRP = OFF               // Configuration Segment Write-Protect bit (Configuration Segment may be written)
+#pragma config CSS = DISABLED           // Configuration Segment Code-Protect Level bits (No Protection (other than CWRP))
+#pragma config AIVTDIS = OFF            // Alternate Interrupt Vector Table bit (Disabled AIVT)
+
+// FBSLIM
+#pragma config BSLIM = 8191             // Boot Segment Flash Page Address Limit bits->
+
+// FOSCSEL
+#pragma config FNOSC = FRCPLL           // Oscillator Source Selection (Fast RC Oscillator with divide-by-N with PLL module (FRCPLL) )
+#pragma config IESO = ON                // Two-speed Oscillator Start-up Enable bit (Start up device with FRC, then switch to user-selected oscillator source)
+
+// FOSC
+#pragma config POSCMD = NONE            // Primary Oscillator Mode Select bits (Primary Oscillator disabled)
+#pragma config OSCIOFNC = ON            // OSC2 Pin Function bit (OSC2 is clock output)
+#pragma config IOL1WAY = OFF            // Peripheral pin select configuration bit (Allow only one reconfiguration)
+#pragma config FCKSM = CSECME           // Clock Switching Mode bits (Clock switching is enabled,Fail-safe Clock Monitor is disabled)
+#pragma config PLLKEN = ON              // PLL Lock Enable Bit (Clock switch will not wait for the PLL lock signal)
+
+// FWDT
+#pragma config WDTPOST = PS1            // Watchdog Timer Postscaler bits (1:1)
+#pragma config WDTPRE = PR32            // Watchdog Timer Prescaler bit (1:32)
+#pragma config WDTEN = OFF        // Watchdog Timer Enable bits (WDT and SWDTEN enabled)
+#pragma config WINDIS = OFF             // Watchdog Timer Window Enable bit (Watchdog Timer in Non-Window mode)
+#pragma config WDTWIN = WIN75           // Watchdog Timer Window Select bits (WDT Windo is 75% of WDT period)
+
+// FICD
+#pragma config ICS = PGD3               // ICD Communication Channel Select bits (Communicate on PGEC3 and PGED3)
+#pragma config JTAGEN = OFF             // JTAG Enable bit (JTAG is disabled)
+
+// FDEVOPT
+#pragma config PWMLOCK = OFF            // PWMx Lock Enable bit (PWM registers may be written without key sequence)
+
+// FALTREG
+#pragma config CTXT1 = IPL5				// Specifies Interrupt Priority Level (IPL) Associated to Alternate Working Register 1 bits (Not Assigned)
+#pragma config CTXT2 = OFF              // Specifies Interrupt Priority Level (IPL) Associated to Alternate Working Register 2 bits (Not Assigned)
+
+
+#elif defined (__P33SMPS_EP5__)
+
+//------------------------------------------------------------------------------
+// DSPIC33EPxxGS502 Configuration Bit Settings
+//------------------------------------------------------------------------------
+// FSEC
+#pragma config BWRP = OFF               // Boot Segment Write-Protect bit (Boot Segment may be written)
+#pragma config BSS = DISABLED           // Boot Segment Code-Protect Level bits (No Protection (other than BWRP))
+#pragma config BSEN = OFF               // Boot Segment Control bit (No Boot Segment)
+#pragma config GWRP = OFF               // General Segment Write-Protect bit (General Segment may be written)
+#pragma config GSS = DISABLED           // General Segment Code-Protect Level bits (No Protection (other than GWRP))
+#pragma config CWRP = OFF               // Configuration Segment Write-Protect bit (Configuration Segment may be written)
+#pragma config CSS = DISABLED           // Configuration Segment Code-Protect Level bits (No Protection (other than CWRP))
+#pragma config AIVTDIS = OFF            // Alternate Interrupt Vector Table bit (Disabled AIVT)
+
+// FBSLIM
+//#pragma config BSLIM = BSLIM            // Boot Segment Flash Page Address Limit bits (Boot Segment Flash page address  limit)
+
+// FOSCSEL
+#pragma config FNOSC = FRCPLL           // Oscillator Source Selection (Fast RC Oscillator with divide-by-N with PLL module (FRCPLL) )
+#pragma config IESO = ON                // Two-speed Oscillator Start-up Enable bit (Start up device with FRC, then switch to user-selected oscillator source)
+
+// FOSC
+#pragma config POSCMD = NONE            // Primary Oscillator Mode Select bits (Primary Oscillator disabled)
+#pragma config OSCIOFNC = ON            // OSC2 Pin Function bit (OSC2 is clock output)
+#pragma config IOL1WAY = OFF            // Peripheral pin select configuration bit (Allow only one reconfiguration)
+#pragma config FCKSM = CSECMD           // Clock Switching Mode bits (Clock switching is enabled,Fail-safe Clock Monitor is disabled)
+#pragma config PLLKEN = ON              // PLL Lock Enable Bit (Clock switch will not wait for the PLL lock signal)
+
+// FWDT
+#pragma config WDTPOST = PS1            // Watchdog Timer Postscaler bits (1:1)
+#pragma config WDTPRE = PR32            // Watchdog Timer Prescaler bit (1:32)
+#pragma config WDTEN = SWDTEN           // Watchdog Timer Enable bits (WDT and SWDTEN disabled)
+#pragma config WINDIS = OFF             // Watchdog Timer Window Enable bit (Watchdog Timer in Non-Window mode)
+#pragma config WDTWIN = WIN75           // Watchdog Timer Window Select bits (WDT Windo is 75% of WDT period)
+
+// FICD
+#pragma config ICS = PGD3               // ICD Communication Channel Select bits (Communicate on PGEC3 and PGED3)
+#pragma config JTAGEN = OFF             // JTAG Enable bit (JTAG is disabled)
+#pragma config BTSWP = OFF              // BOOTSWP Instruction Enable/Disable bit (BOOTSWP instruction is disabled)
+
+// FDEVOPT
+#pragma config PWMLOCK = OFF            // PWMx Lock Enable bit (PWM registers may be written without key sequence)
+#pragma config ALTI2C1 = OFF            // Alternate I2C1 Pin bit (I2C1 mapped to SDA1/SCL1 pins)
+#pragma config ALTI2C2 = OFF            // Alternate I2C2 Pin bit (I2C2 mapped to SDA2/SCL2 pins)
+#pragma config DBCC = OFF               // DACx Output Cross Connection bit (No Cross Connection between DAC outputs)
+
+// FALTREG
+#pragma config CTXT1 = OFF				// Specifies Interrupt Priority Level (IPL) Associated to Alternate Working Register 1 bits (Not Assigned)
+#pragma config CTXT2 = OFF              // Specifies Interrupt Priority Level (IPL) Associated to Alternate Working Register 2 bits (Not Assigned)
+
+
+
+#elif defined (__P33SMPS_EP7__)
+
+//------------------------------------------------------------------------------
+// DSPIC33EPxxGS502 Configuration Bit Settings
+//------------------------------------------------------------------------------
+// FSEC
+#pragma config BWRP = OFF    // Boot Segment Write-Protect bit->Boot Segment may be written
+#pragma config BSS = DISABLED    // Boot Segment Code-Protect Level bits->No Protection (other than BWRP)
+#pragma config BSEN = OFF    // Boot Segment Control bit->No Boot Segment
+#pragma config GWRP = OFF    // General Segment Write-Protect bit->General Segment may be written
+#pragma config GSS = DISABLED    // General Segment Code-Protect Level bits->No Protection (other than GWRP)
+#pragma config CWRP = OFF    // Configuration Segment Write-Protect bit->Configuration Segment may be written
+#pragma config CSS = DISABLED    // Configuration Segment Code-Protect Level bits->No Protection (other than CWRP)
+#pragma config AIVTDIS = OFF    // Alternate Interrupt Vector Table bit->Disabled AIVT
+
+// FBSLIM
+#pragma config BSLIM = 8191    // Boot Segment Flash Page Address Limit bits->
+
+// FOSCSEL
+#pragma config FNOSC = FRCPLL           // Oscillator Source Selection (Fast RC Oscillator with divide-by-N with PLL module (FRCPLL) )
+#pragma config IESO = OFF               // Two-speed Oscillator Start-up Enable bit (Start up device with FRC, then switch to user-selected oscillator source)
+
+// FOSC
+#pragma config POSCMD = NONE    // Primary Oscillator Mode Select bits->Primary Oscillator disabled
+#pragma config OSCIOFNC = ON    // OSC2 Pin Function bit->OSC2 is general purpose digital I/O pin
+#pragma config IOL1WAY = OFF    // Peripheral pin select configuration bit->Allow multiple reconfigurations
+#pragma config FCKSM = CSECME    // Clock Switching Mode bits->Both Clock switching and Fail-safe Clock Monitor are enabled
+#pragma config PLLKEN = ON_PLL    // PLL Lock Enable Bit->Clock switch to PLL source will wait until the PLL lock signal is valid
+
+// FWDT
+#pragma config WDTPOST = PS32768    // Watchdog Timer Postscaler bits->1:32768
+#pragma config WDTPRE = PR128    // Watchdog Timer Prescaler bit->1:128
+#pragma config WDTEN = SWDTEN    // Watchdog Timer Enable bits->WDT Enabled/Disabled (controlled using SWDTEN bit)
+#pragma config WINDIS = OFF    // Watchdog Timer Window Enable bit->Watchdog Timer in Non-Window mode
+#pragma config WDTWIN = WIN25    // Watchdog Timer Window Select bits->WDT Window is 25% of WDT period
+
+// FICD
+#pragma config ICS = PGD1               // ICD Communication Channel Select bits (Communicate on PGEC3 and PGED3)
+#pragma config JTAGEN = OFF             // JTAG Enable bit (JTAG is disabled)
+#pragma config BTSWP = OFF              // BOOTSWP Instruction Enable/Disable bit (BOOTSWP instruction is disabled)
+
+// FDEVOPT
+#pragma config PWMLOCK = OFF            // PWMx Lock Enable bit (PWM registers may be written without key sequence)
+#pragma config ALTI2C1 = OFF            // Alternate I2C1 Pin bit (I2C1 mapped to SDA1/SCL1 pins)
+#pragma config ALTI2C2 = OFF            // Alternate I2C2 Pin bit (I2C2 mapped to SDA2/SCL2 pins)
+#pragma config DBCC = OFF               // DACx Output Cross Connection bit (No Cross Connection between DAC outputs)
+
+// FALTREG
+#pragma config CTXT1 = IPL5             // Specifies Interrupt Priority Level (IPL) Associated to Alternate Working Register 1 bits (Not Assigned)
+#pragma config CTXT2 = OFF              // Specifies Interrupt Priority Level (IPL) Associated to Alternate Working Register 2 bits (Not Assigned)
+#pragma config CTXT3 = OFF              // Specifies Interrupt Priority Level (IPL) Associated to Alternate Working Register 2 bits (Not Assigned)
+#pragma config CTXT4 = OFF              // Specifies Interrupt Priority Level (IPL) Associated to Alternate Working Register 2 bits (Not Assigned)
+
+
+#else
+
+#error === selected device not supported (no configuration bits defined) ===
+
+#endif
